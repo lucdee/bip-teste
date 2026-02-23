f950e46bf2b0544fd0aa054de902e7ab2e90eefd
Lucas Thomazella <62112268+lucdee@users.noreply.github.com>
2026-02-23 17:25:43 -0300

    Inclui coluna VERSION no seed da tabela BENEFICIO

--- backend-module/src/main/resources/data.sql
+++ backend-module/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO BENEFICIO (NOME, DESCRICAO, VALOR, ATIVO) VALUES
-('Beneficio A', 'Descrição A', 1000.00, TRUE),
-('Beneficio B', 'Descrição B', 500.00, TRUE);
+INSERT INTO BENEFICIO (NOME, DESCRICAO, VALOR, ATIVO, VERSION) VALUES
+('Beneficio A', 'Descrição A', 1000.00, TRUE, 0),
+('Beneficio B', 'Descrição B', 500.00, TRUE, 0);
